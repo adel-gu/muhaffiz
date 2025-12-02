050982742d1f639a706f3bda91795c66cf4b81b5
adel <adeelguitoun@gmail.com>
2025-12-02 11:56:40 +0100

    Fix formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button';
 
 function page() {
   return (
     <div>
       <Button>Start session</Button>
     </div>
-  )
+  );
 }
-export default page
+export default page;
