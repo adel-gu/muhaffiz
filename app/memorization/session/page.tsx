95dacc922e23c40ad5b45ccdfd2ab8f0440f3db7
adel <adeelguitoun@gmail.com>
2025-12-04 11:38:38 +0100

    Add header component with settings form

---
+++ app/memorization/session/page.tsx
@@ -0,0 +1,14 @@
+import { Header } from '@/components/session/header';
+
+export default function page() {
+  return (
+    <main>
+      <Header />
+      <div>
+        <div>PROGRESS</div>
+        <div>VERSES</div>
+      </div>
+      <div>ACTIONS</div>
+    </main>
+  );
+}
