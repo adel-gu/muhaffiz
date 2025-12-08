41e32b7965d9699424e78fb58e410b464c56a047
adel <adeelguitoun@gmail.com>
2025-12-08 11:22:32 +0100

    Optimize data fetching and uses propr cache

--- lib/quran-foundation-api/data.ts
+++ lib/quran-foundation-api/data.ts
@@ -1,20 +1,28 @@
 import { quranClient, localQuranClient } from '@/lib/quran-foundation-api';
 import { ChapterId, VerseKey } from '@quranjs/api';
-import { cache } from 'react';
+import { unstable_cache } from 'next/cache';
 
-export const getChapters = cache(async () => {
-  return await quranClient.chapters.findAll();
-});
+// The Quran structure doesn't change, so this should be cached forever (or for a year).
+export const getChapters = unstable_cache(
+  async () => {
+    return await quranClient.chapters.findAll();
+  },
+  ['all-chapters'], // Cache Key
+  { revalidate: false, tags: ['chapters'] },
+);
 
-export const getReciters = cache(async () => {
-  const recitations = await quranClient.resources.findAllRecitations();
-  return recitations.sort((a, b) => Number(a.id ?? 0) - Number(b.id ?? 0));
-});
+export const getReciters = unstable_cache(
+  async () => {
+    const recitations = await quranClient.resources.findAllRecitations();
+    return recitations.sort((a, b) => Number(a.id ?? 0) - Number(b.id ?? 0));
+  },
+  ['all-reciters'],
+  { revalidate: 604800, tags: ['reciters'] },
+);
 
-export const getVerses = cache(
+export const getVerses = unstable_cache(
   async (chapterId: ChapterId, startVerse: number, endVerse: number) => {
     const keys = [] as VerseKey[];
-
     for (let v = startVerse; v <= endVerse; v++) {
       keys.push(`${chapterId}:${v}` as VerseKey);
     }
@@ -23,21 +31,21 @@ export const getVerses = cache(
       keys.map((key) =>
         quranClient.verses.findByKey(key, {
           words: true,
-          fields: {
-            textUthmani: true,
-          },
+          fields: { textUthmani: true },
         }),
       ),
     );
 
     return verses;
   },
+  ['verses-range'],
+  { revalidate: 86400, tags: ['selected-verses'] },
 );
 
-export const getVersesAudio = cache(
+export const getVersesAudio = unstable_cache(
   async ({ chapterId, recitationId }: { chapterId: string; recitationId: string }) => {
-    const data = await localQuranClient.findChapterRecitationById(chapterId, recitationId);
-
-    return data;
+    return await localQuranClient.findChapterRecitationById(chapterId, recitationId);
   },
+  ['verses-audio'],
+  { revalidate: 604800, tags: ['selected-verses-audio'] },
 );
